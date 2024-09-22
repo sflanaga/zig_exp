87f604a62d299c3e6a21f0e2a5d1d917f9d57dc8
sflan <stevemflanagan@gmail.com>
2024-09-22 05:18:19 -0500

    put iterator error handing in the while clause

--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,24 @@
 const std = @import("std");
-const printe = std.debug.print;
-// const print = printe;
 
-var stdbuf = std.io.bufferedWriter(std.io.getStdOut().writer());
-
-// const stdwr = stdbuf.writer();
+var stdOBuf = std.io.bufferedWriter(std.io.getStdOut().writer());
+var stdEBuf = std.io.bufferedWriter(std.io.getStdErr().writer());
 
 pub fn flush_std_io() void {
-    stdbuf.flush() catch |err| {
-        std.debug.panic("Unable to flush std io buffer, error: {}\n", .{err});
+    stdEBuf.flush() catch |err| {
+        std.debug.panic("Unable to flush std err io buffer, error: {}\n", .{err});
+    };
+    stdOBuf.flush() catch |err| {
+        std.debug.panic("Unable to flush std out io buffer, error: {}\n", .{err});
     };
 }
 pub fn print(comptime format: []const u8, args: anytype) void {
-    stdbuf.writer().print(format, args) catch |err| {
+    stdOBuf.writer().print(format, args) catch |err| {
+        std.debug.panic("Unable to write to standard out, so panic {any}\n", .{err});
+    };
+}
+
+pub fn printe(comptime format: []const u8, args: anytype) void {
+    stdEBuf.writer().print(format, args) catch |err| {
         std.debug.panic("Unable to write to standard out, so panic {any}\n", .{err});
     };
 }
@@ -44,12 +50,10 @@ pub fn main() !void {
 
     const absPath = try std.fs.cwd().realpath(dirPath, &outBuf);
 
-    try recursePath(absPath, 0);
-
-    // recursePath(absPath, 0) catch |err| {
-    //     printe("Top level failure: {any}\n", .{err});
-    //     return;
-    // };
+    recursePath(absPath, 0) catch |err| {
+        printe("Top level failure: {any}\n", .{err});
+        return;
+    };
 
     return;
 }
@@ -63,31 +67,25 @@ pub fn recursePath(fullPathDir: []u8, depth: i32) !void {
 
     var iterator = dir.iterate();
     var pathBuf: [4096]u8 = undefined;
-    while (true) {
-        if (iterator.next()) |entryOpt| {
-            if (entryOpt) |entry| {
-                const entryType = switch (entry.kind) {
-                    .file => "F",
-                    .directory => "D",
-                    else => continue,
-                };
-                // const abs_path = try std.fs.realpath(entry.name, &out_buf);
-                const absPath = dir.realpath(entry.name, &pathBuf) catch |err| {
-                    // note this error does happen, and is not strictly necessary - bug?
-                    // but zig is singular is provided a system call way of simply catting a path - just bizare
-                    printe("error: create full path error on entry \"{s}\" under dir: \"{s}\" due to error: {}\n", .{ entry.name, fullPathDir, err });
-                    continue;
-                };
-                print("{d} {s}: {s}\n", .{ depth, entryType, absPath });
-                if (entry.kind == std.fs.Dir.Entry.Kind.directory) {
-                    try recursePath(absPath, depth + 1);
-                }
-            } else {
-                break;
-            }
-        } else |err| {
-            printe("error on next directory item under {s}, so giving up on it, error {}", .{ fullPathDir, err });
-            return;
+    while (iterator.next() catch |err| {
+        printe("error on next directory item under {s}, so giving up on it, error {}\n", .{ fullPathDir, err });
+        return;
+    }) |entry| {
+        const entryType = switch (entry.kind) {
+            .file => "F",
+            .directory => "D",
+            else => continue,
+        };
+        // const abs_path = try std.fs.realpath(entry.name, &out_buf);
+        const absPath = dir.realpath(entry.name, &pathBuf) catch |err| {
+            // note this error does happen, and is not strictly necessary - bug?
+            // but zig is singular is provided a system call way of simply catting a path - just bizare
+            printe("error: create full path error on entry \"{s}\" under dir: \"{s}\" due to error: {}\n", .{ entry.name, fullPathDir, err });
+            continue;
+        };
+        print("{d} {s}: {s}\n", .{ depth, entryType, absPath });
+        if (entry.kind == std.fs.Dir.Entry.Kind.directory) {
+            try recursePath(absPath, depth + 1);
         }
     }
     return;
